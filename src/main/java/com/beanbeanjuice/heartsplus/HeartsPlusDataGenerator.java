5e251a5d02a81dcfb848bb79bf1e779e2ce8ceed
beanbeanjuice <beanbeanjuice@outlook.com>
2025-03-31 17:19:51 -0700

    Hello, world!
    Initial commit.

---
+++ src/main/java/com/beanbeanjuice/heartsplus/HeartsPlusDataGenerator.java
@@ -0,0 +1,13 @@
+package com.beanbeanjuice.heartsplus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HeartsPlusDataGenerator implements DataGeneratorEntrypoint {
+
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+
+}
